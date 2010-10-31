3e68e1fedcad4b9e02ded25edd6d9a0d0f058d70
Matt Weight <mattweight@gmail.com>
2010-10-31 15:27:08 -0600

    Keeping it all up-to-date

---
+++ prototype_bubble/Classes/WordView.h
@@ -0,0 +1,16 @@
+//
+//  WordView.h
+//  BubblePOC
+//
+//  Created by Matt Weight on 10/31/10.
+//  Copyright 2010 Digitaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WordView : UIView {
+
+}
+
+@end
---
+++ prototype_bubble/Classes/WordViewController.h
@@ -0,0 +1,16 @@
+//
+//  WordViewController.h
+//  BubblePOC
+//
+//  Created by Matt Weight on 10/31/10.
+//  Copyright 2010 Digitaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WordViewController : UIViewController {
+
+}
+
+@end
